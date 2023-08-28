d6fc8bc504dafea12d3f038bc27fbbb39fa8de39
deividferraz20@gmail.com <deividferraz20@gmail.com>
2023-08-28 01:41:55 -0300

    projeto MongoDb inicializado

---
+++ src/test/java/project/db/WorkshopmongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package project.db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
